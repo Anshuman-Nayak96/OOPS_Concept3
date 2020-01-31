9fadb5e9633d1560a1c4c4e03a59daaa28d60917
pcuser <pcuser@INFCH03372>
2020-01-31 13:02:44 +0530

    Add project files.

---
+++ OOPS_Concept3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concept3
+{
+    class Parent
+    {
+        internal string name="Parent";
+    }
+    class child:Parent
+    {
+        string name="Child";
+        internal void display()
+        {
+            Console.WriteLine("The parent class name is "+base.name+"  "+ "The child class name is "+name);
+            Console.Read();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var? i = null;
+            //var i, j;
+            //var i = 10, j;
+            child obj = new child();
+            obj.display();
+        }
+    }
+}
